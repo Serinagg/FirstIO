b7d218fb4ff21680b321379a454573ce157121fd
serinagg <sgarcia14@mail.csuchico.edu>
2018-09-05 16:19:52 -0700

    changes to greeting

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <string>
 
+//Author: Serina Garcia
+
 int main() {
     std::string input;
-    std::cout << "What's your first name?" << std::endl;
+    std::cout << "Hi, my name is Serina. What's your first name?" << std::endl;
     std::cin >> input;
     std::cout << "Hello, " << input << std::endl;
     return 0;
